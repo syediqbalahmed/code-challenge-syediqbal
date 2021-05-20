388c3b28c98c132dd34bf61f3e79f44c8123f39c
Syed Iqbal Ahmed <iqqi5192@gmail.com>
2021-05-20 12:27:11 +0530

    Addded helm and alb-ingress provider in main.tf to be provisioned

--- main.tf
+++ main.tf
@@ -72,3 +72,33 @@ module "eks" {
   write_kubeconfig   = true
   config_output_path = "./"
 }
+
+provider "helm" {
+  version = "1.3.1"
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    load_config_file       = false
+  }
+}
+
+resource "helm_release" "ingress" {
+  name       = "ingress"
+  chart      = "aws-alb-ingress-controller"
+  repository = "https://charts.helm.sh/incubator"
+  version    = "1.0.2"
+
+  set {
+    name  = "autoDiscoverAwsRegion"
+    value = "true"
+  }
+  set {
+    name  = "autoDiscoverAwsVpcID"
+    value = "true"
+  }
+  set {
+    name  = "clusterName"
+    value = local.cluster_name
+  }
+}
